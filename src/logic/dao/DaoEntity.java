a2da1f6bb5950df0fcd64095ee0540cbbcd0139f
valeriocristofori <56649922+ValerioCristofori@users.noreply.github.com>
2020-07-09 13:46:30 +0200

    Update DaoEntity.java

--- src/logic/dao/DaoEntity.java
+++ src/logic/dao/DaoEntity.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 public class DaoEntity {
 	private static DaoEntity instance = null; 
 	protected static String user = "root";
-	protected static String pass = "zurigo70";
+	protected static String pass = "";
 	protected static String dbUrl = "jdbc:mysql://localhost:3306/TheFridgeDB";
 	protected static String driverClassName = "com.mysql.cj.jdbc.Driver";
 	protected static String databaseName = "TheFridgeDB";
